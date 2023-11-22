48f109287f3b87a62ff540d62c38d21edba750d8
karthik <karcse17@gmail.com>
2023-11-22 12:05:44 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Welcome Ranjitha',
+        'name' => 'Welcome Simple PHP Website Done',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
