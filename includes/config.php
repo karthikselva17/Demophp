3e99e0bd5adfe990a80abfcbb9f64c766901c16e
karthik <karcse17@gmail.com>
2023-11-22 09:44:26 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website Completed',
+        'name' => 'Welcome Bakiya',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
